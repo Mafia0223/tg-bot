0b01b9f9a253e6f669b1b162e955c46cdbde3aa1
unknown <namba@example.com>
2026-05-05 21:20:07 +0600

    fix syntax

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-```python
 from flask import Flask, request
 import telebot
 from telebot import types
@@ -276,4 +275,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
-```
